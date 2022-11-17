ea4437dcd5414e0b0d26a475cd9e7854cd9f51a1
Taylor Hiatt <taylor@hiatt.dev>
2022-11-17 12:41:50 -0500

    few notes and get the crates setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 /*
-TODO: idk maybe make it or something
+Grape Soda is a general purpose Rust library for common util function I use when making my projects.
+Contributions are accepted, so if you want to make improvements, send in a pull request.
 
 
+Project Structure:
+    The library is split into different private crates and the workspace root provides the implementation of the actual library methods, using
+    implementations defined in the private crates.
 */
+
